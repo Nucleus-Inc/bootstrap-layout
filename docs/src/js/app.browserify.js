072b8da0dd2f74a03baa19a83943dd675c2ef70e
Laza Bogdan <laza.bogdan@macro-net.ro>
2016-05-10 10:55:50 +0300

    update docs

--- docs/src/js/app.browserify.js
+++ docs/src/js/app.browserify.js
@@ -1,18 +1,18 @@
 // Vendor libraries
 // jQuery
-window.$ = window.jQuery = require('jquery');
+window.$ = window.jQuery = require('jquery')
 
 // Tether (required by Bootstrap 4)
-window.Tether = require('tether');
+window.Tether = require('tether')
 
 // Bootstrap 4
-require('bootstrap');
+require('bootstrap')
 
 // Simplebar
-require('simplebar/dist/simplebar.min');
+require('simplebar/dist/simplebar.min')
 
 // Bootstrap Layout
-require('bootstrap-layout');
+require('bootstrap-layout')
 
 // Bootstrap Layout Scrollable
 require('bootstrap-layout-scrollable')
